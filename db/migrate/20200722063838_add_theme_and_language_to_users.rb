853bfddbe97af790fc95746c37063b2901b16b29
kyawthetaung-sg <softguide.kyawthetaung@gmail.com>
2020-07-22 18:49:54 +0900

    remove theme_id default 1 in user table for fix heroku

--- db/migrate/20200722063838_add_theme_and_language_to_users.rb
+++ db/migrate/20200722063838_add_theme_and_language_to_users.rb
@@ -1,6 +1,6 @@
 class AddThemeAndLanguageToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :theme, null: false, foreign_key: true, default: 1
+    add_reference :users, :theme, foreign_key: true
     add_column :users, :language, :int, null: false, default: 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_063838) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "theme_id", default: 1, null: false
+    t.integer "theme_id"
     t.integer "language", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
