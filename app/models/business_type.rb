12f21ea6fdf73f3247cde11384c3eff645906034
kyawthetaung-sg <softguide.kyawthetaung@gmail.com>
2020-09-14 00:18:13 +0900

    add paginate

--- app/models/business_type.rb
+++ app/models/business_type.rb
@@ -1,6 +1,8 @@
 class BusinessType < ApplicationRecord
   include Filterable
 
+  paginates_per 25
+
   validates :name, presence: true
   validates :category_id, presence: true
   validates :purchase_price, presence: true
