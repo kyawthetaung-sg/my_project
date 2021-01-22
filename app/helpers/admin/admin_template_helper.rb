03f0eeeadd5ac25ed5f23e6c823c914fe291aea0
kyawthetaung-sg <softguide.kyawthetaung@gmail.com>
2021-01-22 12:58:24 +0900

    edit permission

--- app/helpers/admin/admin_template_helper.rb
+++ app/helpers/admin/admin_template_helper.rb
@@ -1,40 +1,40 @@
 module Admin::AdminTemplateHelper
     def user_management_links
         options = [
-            {condition: true, path: accounts_path, icon_class: "far fa-circle", label: t("sub_sidebar.user_management.account")},
-            {condition: true, path: roles_path, icon_class: "far fa-circle", label: t("sub_sidebar.user_management.role")},
+            {condition: has_permission("account_read"), path: accounts_path, icon_class: "far fa-circle", label: t("sub_sidebar.user_management.account")},
+            {condition: has_permission("role_read"), path: roles_path, icon_class: "far fa-circle", label: t("sub_sidebar.user_management.role")},
         ]
         main_dropdown_link(t("main_sidebar.user_management"), "fas fa-user", "userSubmenu", options)
     end
 
     def business_links
         options = [
-            {condition: true, path: product_sales_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product_sale")},
-            {condition: true, path: sale_lists_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.sale_list")},
-            {condition: true, path: product_sale_lists_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product_sale_list")},
-            {condition: true, path: capitals_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.capital")},
-            {condition: true, path: debt_capitals_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.debt_capital")},
-            {condition: true, path: general_expenses_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.general_expense")},
-            {condition: true, path: categories_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.category")},
-            {condition: true, path: business_types_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product")},
-            {condition: true, path: customers_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.customer")},
+            {condition: has_permission("product_sale_read"), path: product_sales_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product_sale")},
+            {condition: has_permission("sale_list_read"), path: sale_lists_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.sale_list")},
+            {condition: has_permission("product_sale_list_read"), path: product_sale_lists_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product_sale_list")},
+            {condition: has_permission("debt_capital_read"), path: capitals_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.capital")},
+            {condition: has_permission("capital_read"), path: debt_capitals_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.debt_capital")},
+            {condition: has_permission("general_expense_read"), path: general_expenses_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.general_expense")},
+            {condition: has_permission("business_category_read"), path: categories_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.category")},
+            {condition: has_permission("product_read"), path: business_types_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.product")},
+            {condition: has_permission("customer_read"), path: customers_path, icon_class: "far fa-circle", label: t("sub_sidebar.business.customer")},
         ]
         main_dropdown_link(t("main_sidebar.business"), "fas fa-briefcase", "businessSubmenu", options)
     end
 
     def expense_links
         options = [
-            {condition: true, path: expense_categories_path, icon_class: "far fa-circle", label: "Category"},
-            {condition: true, path: payment_modes_path, icon_class: "far fa-circle", label: "Payment Mode"},
-            {condition: true, path: incomes_path, icon_class: "far fa-circle", label: "Income"},
-            {condition: true, path: expenses_path, icon_class: "far fa-circle", label: "Expense"},
+            {condition: has_permission("expense_category_read"), path: expense_categories_path, icon_class: "far fa-circle", label: t("sub_sidebar.expense.category")},
+            {condition: has_permission("payment_mode_read"), path: payment_modes_path, icon_class: "far fa-circle", label: t("sub_sidebar.expense.payment_mode")},
+            {condition: has_permission("income_read"), path: incomes_path, icon_class: "far fa-circle", label: t("sub_sidebar.expense.income")},
+            {condition: has_permission("expense_read"), path: expenses_path, icon_class: "far fa-circle", label: t("sub_sidebar.expense.expense")},
         ]
         main_dropdown_link(t("main_sidebar.expense")+" (#{ current_user.user_name })", "fas fa-wallet", "expenseSubmenu", options)
     end
 
     def site_setting_links
         options = [
-            {condition: true, path: themes_path, icon_class: "far fa-circle", label: t("sub_sidebar.setting.theme")},
+            {condition: has_permission("theme_read"), path: themes_path, icon_class: "far fa-circle", label: t("sub_sidebar.setting.theme")},
         ]
         main_dropdown_link(t("main_sidebar.setting"), "fas fa-cog", "settingSubmenu", options)
     end
@@ -96,6 +96,15 @@ module Admin::AdminTemplateHelper
             language_name = "Japanese"
         else
             language_name = "English"
+        end
+    end
+
+    def has_permission(name)
+        role = current_user.role
+        if role.id == 1
+            return true
+        else
+            role.permissions.pluck(:name).include? name
         end
     end
 
